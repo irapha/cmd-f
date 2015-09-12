1cc992f4f729da154053a0dfa5a16a9e3569322a
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2015-09-12 03:46:37 -0400

    add tessdata files again.

---
+++ Cmd-F/TesseractBridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  TesseractBridgingHeader.h
+//  Cmd-F
+//
+//  Created by Raphael Gontijo Lopes on 12/9/15.
+//  Copyright (c) 2015 MHackers. All rights reserved.
+//
+
+#ifndef Cmd_F_TesseractBridgingHeader_h
+#define Cmd_F_TesseractBridgingHeader_h
+
+#import <TesseractOCR/TesseractOCR.h>
+
+#endif
--- Cmd-F/ViewController.swift
+++ Cmd-F/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var tesseract:G8Tesseract = G8Tesseract(language:"eng");
+        
     }
 
     override func didReceiveMemoryWarning() {
