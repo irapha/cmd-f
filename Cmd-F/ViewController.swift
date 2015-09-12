74908a03852f5d6d8636f102d3dd58b2113a89a1
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2015-09-12 04:30:17 -0400

    Tesseract working on a sample image.

--- Cmd-F/ViewController.swift
+++ Cmd-F/ViewController.swift
@@ -8,14 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, G8TesseractDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         var tesseract:G8Tesseract = G8Tesseract(language:"eng")
-        // tesseract.delegate = self
+        tesseract.delegate = self
+        tesseract.image = UIImage(named: "image_sample.jpg")
+        tesseract.recognize()
         
+        NSLog("%@", tesseract.recognizedText);
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +26,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func shouldCancelImageRecognitionForTesseract(tesseract: G8Tesseract!) -> Bool {
+        return false; // return true if you need to interrupt tesseract before it finishes
+    }
 }
 
