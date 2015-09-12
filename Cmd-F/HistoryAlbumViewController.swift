25553644a6202571e8406edb7ee97e747e7388ff
Alexzander Gerlach <zander.salamander@yahoo.com>
2015-09-12 19:01:37 -0400

    Further fix

---
+++ Cmd-F/HistoryAlbumViewController.swift
@@ -0,0 +1,72 @@
+//
+//  HistoryAlbumViewController.swift
+//  Cmd-F
+//
+//  Created by Apple on 9/12/15.
+//  Copyright (c) 2015 MHackers. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+
+class HistoryAlbumViewController: UIViewController{
+
+    @IBOutlet var scrollView: UIScrollView!
+    
+    
+    var images: [UIImage] = []
+    var frame: CGRect = CGRectMake(0, 0, 0, 0)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for index in (0..<images.count) {
+            
+            frame.origin.x = self.scrollView.frame.size.width * CGFloat(index)
+            frame.size = self.scrollView.frame.size
+            self.scrollView.pagingEnabled = true
+            
+            var subView = UIImageView(frame: frame)
+            subView.image = images[index]
+            self.scrollView .addSubview(subView)
+        
+        }
+        
+        self.scrollView.contentSize = CGSize(width: self.scrollView.frame.size.width * CGFloat(images.count), height: self.scrollView.frame.size.height)
+    }
+}
+//
+//    
+//    var historyArray: [HistoryObject]?
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        
+//        let rightRecog = UISwipeGestureRecognizer(target: self, action: "swipeRight")
+//        rightRecog.direction = UISwipeGestureRecognizerDirection.Right
+//        self.view.addGestureRecognizer(rightRecog)
+//        
+//        let leftRecog = UISwipeGestureRecognizer(target: self, action: "swipeLeft")
+//        leftRecog.direction = UISwipeGestureRecognizerDirection.Left
+//        self.view.addGestureRecognizer(leftRecog)
+//    }
+//    
+//    func swipeRight() {
+//        performSegueWithIdentifier("rightSwipe", sender: self)
+//    }
+//    
+//    func swipeLeft() {
+//        performSegueWithIdentifier("leftSwipe", sender: self)
+//    }
+//    
+//
+//    let historyObject.historyImage = UIImage
+//    let imageView = UIImageView(image: historyImage!)
+//    
+//    let imageView.frame = CGRect(x: 0, y: 0, width: 100, height: 200)
+//    view.addSubview(imageView)
+//}
+
