353a3fd12e51240af545a7ac781b6175b13f05b7
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2015-09-12 04:01:47 -0400

    Add podfiles and whatnot

--- Cmd-F/ViewController.swift
+++ Cmd-F/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        var tesseract:G8Tesseract = G8Tesseract(language:"eng");
+        var tesseract:G8Tesseract = G8Tesseract(language:"eng")
+        tesseract.delegate = self
         
     }
 
