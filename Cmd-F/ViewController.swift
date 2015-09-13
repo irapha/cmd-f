3671b699796341843697481e34e244a99c5528b2
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2015-09-12 22:01:18 -0400

    improve UI, factor out tesseract function (also makes app behavior more intuitive by closing cam view before processing)

--- Cmd-F/ViewController.swift
+++ Cmd-F/ViewController.swift
@@ -24,15 +24,12 @@ class ViewController: UIViewController, G8TesseractDelegate, UIImagePickerContro
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-//        let gesture = UITapGestureRecognizer(target: self, action: "presentCamera")
-//        self.view.addGestureRecognizer(gesture)
         print("init")
         
         // Intialize tesseract.
         tesseract = G8Tesseract(language:"eng")
         tesseract!.delegate = self
-        
+
         // Swipe up on imageview loads the camera view.
         let swipeUp = UISwipeGestureRecognizer(target: self, action: "respondToSwipeUp:")
         swipeUp.direction = .Up
@@ -51,13 +48,9 @@ class ViewController: UIViewController, G8TesseractDelegate, UIImagePickerContro
         picker = createCamera()
         print("view loaded")
         
-        if picker != nil {
-            if firstTimeAppearing == true {
-                firstTimeAppearing = false;
-                 presentViewController(picker!, animated: true, completion: nil)
-            }
-        } else {
-            print("fail")
+        if firstTimeAppearing == true {
+            firstTimeAppearing = false;
+            presentCamera()
         }
     }
     
@@ -65,13 +58,12 @@ class ViewController: UIViewController, G8TesseractDelegate, UIImagePickerContro
         NSBundle.mainBundle().loadNibNamed("OverlayView", owner: self, options: nil)
         let tap = UITapGestureRecognizer(target: self, action: "closeKeyboard")
         overlayView.addGestureRecognizer(tap)
-        cameraButton.layer.cornerRadius = 0.2 * cameraButton.bounds.size.width
+        cameraButton.layer.cornerRadius = cameraButton.bounds.size.width / 2
         cameraButton.backgroundColor = UIColor.whiteColor()
         
         overlayView.frame = picker.cameraOverlayView!.frame
         overlayView.opaque = false
         overlayView.backgroundColor = UIColor.clearColor()
-
     }
     
     func createCamera() -> UIImagePickerController? {
@@ -87,6 +79,17 @@ class ViewController: UIViewController, G8TesseractDelegate, UIImagePickerContro
             picker.toolbarHidden = true
             initializeOverlay(picker)
             picker.cameraOverlayView = overlayView
+            
+            let screenSize = UIScreen.mainScreen().bounds.size
+            let cameraAspectRatio = 4.0 / 3.0
+            let imageWidth = floor(Double(screenSize.width) * cameraAspectRatio)
+            let scale = CGFloat(ceil((Double(screenSize.height) / imageWidth) * 10.0) / 10.0)
+            let translationX = CGFloat(0.0)
+            let translationY = CGFloat(2 * textQuery.bounds.height)
+            
+            picker.cameraViewTransform = CGAffineTransformConcat(CGAffineTransformMakeScale(scale, scale)
+                , CGAffineTransformMakeTranslation(translationX, translationY))
+            
             return picker
         } else {
             return nil
@@ -117,8 +120,6 @@ class ViewController: UIViewController, G8TesseractDelegate, UIImagePickerContro
     }
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
-        print("Starting tesseract")
-    
         // Find ranges in recognizedText where seachQuery matches. Remove all new lines and spaces from both strings (so that the blocks array correspond one-to-one).
         var searchQuery: String
         
@@ -128,8 +129,14 @@ class ViewController: UIViewController, G8TesseractDelegate, UIImagePickerContro
             searchQuery = ""
         }
         
-        dismissViewControllerAnimated(true, completion: nil)
-        
+        dismissViewControllerAnimated(true, completion: {() -> () in
+            self.tesseract(searchQuery, image: image)
+        })
+    }
+
+    func tesseract(searchQuery: String, image: UIImage!) {
+        print("Starting tesseract")
+
         // Give tesseract a preprocessed UIImage.
         tesseract!.image = image.g8_grayScale().g8_blackAndWhite()
         
@@ -137,28 +144,32 @@ class ViewController: UIViewController, G8TesseractDelegate, UIImagePickerContro
         tesseract!.recognize()
         let recognizedText = tesseract!.recognizedText
         
-        let formattedRecognizedText = recognizedText.lowercaseString.stringByReplacingOccurrencesOfString(" ", withString: "").stringByReplacingOccurrencesOfString("\n", withString: "")
-        let formattedSearchQuery = searchQuery.lowercaseString.stringByReplacingOccurrencesOfString(" ", withString: "").stringByReplacingOccurrencesOfString("\n", withString: "")
-        
-        // Get match start and end index.
-        let rangeOfMatch = formattedRecognizedText.rangeOfString(formattedSearchQuery)
-        let matchStartIndex = formattedRecognizedText.startIndex.distanceTo(rangeOfMatch!.startIndex)
-        let matchEndIndex = formattedRecognizedText.startIndex.distanceTo(rangeOfMatch!.endIndex)
-        
-        // Get all recognized character's block
-        var blocks = tesseract!.recognizedBlocksByIteratorLevel(G8PageIteratorLevel.Symbol) as! [G8RecognizedBlock]
-        // Only use blocks that match searchQuery.
-        let filteredBlocks = Array(blocks[matchStartIndex..<matchEndIndex])
-        
-        // Make tesseract display the image with the highlighted blocks.
-        imageView.image = tesseract!.imageWithBlocks(filteredBlocks, drawText: true, thresholded: false)
-        
-        // How to request information from google books.
-        //        let remote = GoogleBooksRemote()
-        //        let query = ("/books/v1/volumes?q=" + recognizedText!.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLHostAllowedCharacterSet())! + "&key=AIzaSyDhY74nCaymN5Slm-doWyoweJrAbLYWJVM")
-        //        NSLog("%@", query)
-        //        remote.connect(query)
+        if searchQuery.characters.count > 0 {
+            // TODO: allow recognition of multiple matches.
+            let formattedRecognizedText = recognizedText.lowercaseString.stringByReplacingOccurrencesOfString(" ", withString: "").stringByReplacingOccurrencesOfString("\n", withString: "")
+            let formattedSearchQuery = searchQuery.lowercaseString.stringByReplacingOccurrencesOfString(" ", withString: "").stringByReplacingOccurrencesOfString("\n", withString: "")
+            
+            // Get match start and end index.
+            let rangeOfMatch = formattedRecognizedText.rangeOfString(formattedSearchQuery)
+            let matchStartIndex = formattedRecognizedText.startIndex.distanceTo(rangeOfMatch!.startIndex)
+            let matchEndIndex = formattedRecognizedText.startIndex.distanceTo(rangeOfMatch!.endIndex)
+            
+            // Get all recognized character's block
+            var blocks = tesseract!.recognizedBlocksByIteratorLevel(G8PageIteratorLevel.Symbol) as! [G8RecognizedBlock]
+            // Only use blocks that match searchQuery.
+            let filteredBlocks = Array(blocks[matchStartIndex..<matchEndIndex])
+            
+            // Make tesseract display the image with the highlighted blocks.
+            imageView.image = tesseract!.imageWithBlocks(filteredBlocks, drawText: true, thresholded: false)
+            
+            // Request information from google books.
+            //            let remote = GoogleBooksRemote()
+            //            let query = ("/books/v1/volumes?q=" + recognizedText!.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLHostAllowedCharacterSet())! + "&key=AIzaSyDhY74nCaymN5Slm-doWyoweJrAbLYWJVM")
+            //            NSLog("%@", query)
+            //            remote.connect(query)
+        } else {
+            // TODO: display a 'warning: no query performed' message.
+        }
     }
-        
 }
 
